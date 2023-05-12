2bda5e274b0f47c9e43ca717466a2c55b4b627cc
star <juniorprogrammer09@gmail.com>
2023-05-12 14:45:24 +0000

    add taxi with owner test

--- src/test/java/com/junrdev/taxisys/repository/TaxiRepoTest.java
+++ src/test/java/com/junrdev/taxisys/repository/TaxiRepoTest.java
@@ -7,15 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class TaxiRepoTest {
 
-    private final TaxiRepo repo;
+    private final TaxiRepo taxiRepo;
+    private final OwnerRepo ownerRepo;
 
     @Autowired
-    public TaxiRepoTest(TaxiRepo repo) {
-        this.repo = repo;
+    public TaxiRepoTest(TaxiRepo taxiRepo, OwnerRepo ownerRepo) {
+        this.taxiRepo = taxiRepo;
+        this.ownerRepo = ownerRepo;
     }
 
     @Test
@@ -23,7 +26,7 @@ public class TaxiRepoTest {
 
     @Test
     void getTaxis(){
-        List<Taxi> taxis = repo.findAll();
+        List<Taxi> taxis = taxiRepo.findAll();
 
         System.out.println("taxis = " + taxis);
     }
@@ -39,8 +42,51 @@ public class TaxiRepoTest {
                 .mileage(Mileage.builder().speed(120).mileagePer(Mileage.mileagePer.MPH).build())
                 .build();
 
-        Taxi saved = repo.save(taxi);
+        Taxi saved = taxiRepo.save(taxi);
 
         System.out.println("saved taxi= " + saved);
+    }
+
+    @Test
+    void addTaxiWithOwner(){
+        Taxi taxi = Taxi.builder()
+                .plateNumber("KCM 974T")
+                .capacity(4)
+                .fuelCapacity(4f)
+                .loadedWeight(2489f)
+                .weightUnloaded(1200f)
+                .mileage(Mileage.builder().speed(120).mileagePer(Mileage.mileagePer.MPH).build())
+                .owner(ownerRepo.findById(1l).get())//can choose to create a new one or find the existing one
+                .build();
+
+        Taxi saved = taxiRepo.save(taxi);
+
+        System.out.println("saved taxi= " + saved);
+    }
+
+    @Test
+    void updateTaxi(){
+        Optional<Taxi> old_taxi = taxiRepo.findById(1L);
+
+        if(old_taxi.isEmpty())
+            throw new RuntimeException("Invalid id provided");
+
+        Taxi taxi = old_taxi.get();
+
+        taxi.setMaxWeight(3000f);
+
+        System.out.println("taxiRepo.save(taxi) = " + taxiRepo.save(taxi));
+    }
+
+    @Test
+    void deleteTaxi(){
+        Optional<Taxi> taxi = taxiRepo.findById(1L);
+
+        if(taxi.isEmpty())
+            throw new RuntimeException("Invalid id provided");
+
+        taxiRepo.delete(taxi.get());
+
+        System.out.println("Deleted taxi");
     }
 }
